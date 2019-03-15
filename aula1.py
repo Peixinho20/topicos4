6de3f96b1f14994d613fc5c9a35f79de2a18c1b2
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-03-15 12:41:33 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,8 @@
+a = float(10/1.61)
+b = float(43.5/60)
+c = float(a/b)
+print('A velocidade média em milhas por hora é',c)
+#A velocidade média em milhas por hora é 8.567144998929106
+d = float(1/c)
+print('o tempo médio por milha é',d,'em horas.')
+#o tempo médio por milha é 0.11672500000000002 em horas.
