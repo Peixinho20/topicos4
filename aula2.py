5127f0911d27817bc321a1b45f620d4170056c0d
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-03-28 16:39:47 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,20 @@
+exercício 3
+n = 60
+l = 24.95
+d = (40/100)*n*l
+c = 3 + 0.75*(n-1)
+t = n*l - d + c
+print('valor total:',t)
+valor total: 945.45
+
+exercício 4
+o = 632.8*10**(-9)
+D = 1.98
+d = 0.250*10**(-3)
+vary = (D*o)/d
+print(vary)
+0.005011776
+
+
+
+refazer o exercício 2 dessa aula
