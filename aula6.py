2b4edc0eeda853704452cbea0ae398ce642b0a83
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-04-05 12:06:59 -0300

    Update aula6.py

--- aula6.py
+++ aula6.py
@@ -29,27 +29,33 @@ teca.pensize(c)
 '''
 4)desenhe um quadrado, usando a forma de tartaruga, ao invés da flecha, para desenhar.
 '''
+#ESSE EXERCÍCIO ESTÁ DANDO RUIM
 teca = turtle.shape("turtle")
 for i in [0,1,2,3]:
     teca.forward(60)#medida de cada lado da figura
     teca.left(90)#faz a tartaruga virar 90°
-jn.mainloop ()
+jn.mainloop () #FINALIZA O PROGRAMA
 
+'''
+4a) mude a velocidade com que a tartaruga faz o desenho
+'''
+
+'''
+5)Sabendo que o ângulo interno da ponta de uma estrela de 5 pontas é de 36 graus, desenhe uma estrela em uma janela. 
+'''
+teca.shape("turtle")
+for i in [0,1,2,3,4]:
+	teca.forward(90)
+	teca.left(144) #essa medida é o suplemento do angulo interno.
+'''    
+6)   Sabendo o ângulo interno da ponta de uma estrela, desenhe quatro estrelas em uma janela, com uma certa distância
+entre elas. Dica: use a função penup() e pendown() do módulo turtle 
+'''
+teca.shape("turtle")
+for i in [0,1,2,3,4]:
+	teca.forward(90)
+	teca.left(144) #essa medida é o suplemento do angulo interno.
+	teca.pendown() #ao executar os novos programas, não aparece o rastro da tartaruga. Ver depois.
+	teca.penup()
 INCOMPLETO
-import turtle
-a = str(input('Digite a cor de fundo desejada: '))
-jn = turtle.Screen()
-jn.bgcolor(a)
 
-b = str(input('Se desejar pode mudar a cor da tratura também: '))
-teca = turtle.Turtle()
-teca.color(b)
-
-c = int(input('Se quiser pode mudar também a largura da caneta: '))
-teca.pensize(c)
-
-teca = turtle.shape("turtle")
-for i in [0,1,2,3]:
-        teca.forward(100) 
-        teca.left(90) 
-jn.mainloop ()
