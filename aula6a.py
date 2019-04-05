7a92ee7c0e6ecfd13cc6aaab6adab798512230a6
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-04-05 12:21:27 -0300

    Create aula6a.py

---
+++ aula6a.py
@@ -0,0 +1,6 @@
+#Exercícios em preparação para os próximos capítulos
+
+'''
+Escreva uma função chamada square, que toma um parâmetro t, que é uma Turtle.Essa função deve usar o turtle para desenhar
+um quadrado. Escreva uma chamada de função que passe joana como argumento para square e rode o programa novamente.
+'''
